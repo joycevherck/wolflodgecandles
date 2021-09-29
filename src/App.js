cdf0d7d2f389b253334663fd77c8ae4eb446f43a
Joyce Van Herck <joyce.vherck@gmail.com>
2021-09-29 10:14:18 +0100

    Hero WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import Hero from './components/hero';
-import Nav from './components/nav';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
       <Hero />
     </div>
   );
--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './styles.scss'
+import Logo from '../../images/logo-black.svg'
 
 const Hero = () => (
   <div className="heroWrapper">
+    <div className="shape" />
+    <div className="logo">
+      <img src={Logo} alt="Wolf Lodge Candles logo" />
+    </div>
     <h1>Wolf Lodge Candles</h1>
     <h2>Vegan and cruelty free handmade soy candles and melts</h2>
+
+    <nav className="buy">
+      <a href="https://www.etsy.com/uk/shop/WolfLodgeCandles">Buy on Etsy</a>
+    </nav>
   </div>
 )
 
